f73ca23a792d016f124b19c74f646ca3260a543b
stripathi <stripathi@GBWK-INA0370LH1.galaxy.lan>
2018-05-22 09:38:59 +0530

    second commit

---
+++ jenkinMavenPack/src/test/java/jenkinMavenPro/jenkinMavenPack/AppTest.java
@@ -0,0 +1,14 @@
+package jenkinMavenPro.jenkinMavenPack;
+
+import org.testng.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		Assert.assertFalse(false);
+	}
+  
+}
